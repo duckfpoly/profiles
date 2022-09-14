dd825827ab8c5a4e92439337d2154884de23951a
duckk <thienduc.nguyen098@gmail.com>
2022-09-14 13:14:20 +0700

    profiles

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Lofi",
+    artist: "English",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "EDM",
+    artist: "Chill",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Lofi Chill Viet",
+    artist: "Chill with me 1",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "List Music",
+    artist: "Đen Vâu",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Lofi Chill Viet",
+    artist: "Chill with me 2",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lofi Chill Viet",
+    artist: "Chill with me 3",
+    img: "music-6",
+    src: "music-6"
+  },
+];
